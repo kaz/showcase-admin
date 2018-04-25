1b0e88986deaa7f2816232a3654bc325a1ced699
Kazuki Sawada <kazuki@6715.jp>
2018-04-25 21:24:18 +0900

    init

---
+++ src/components/Dashboard/Views/Overview.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>Vue paper dashboard starter template</div>
+</template>
+<script>
+  export default {}
+</script>
+<style>
+
+</style>
---
+++ src/routes/routes.js
@@ -0,0 +1,29 @@
+import DashboardLayout from '../components/Dashboard/Layout/DashboardLayout.vue'
+// GeneralViews
+import NotFound from '../components/GeneralViews/NotFoundPage.vue'
+
+// Admin pages
+import Overview from 'src/components/Dashboard/Views/Overview.vue'
+
+const routes = [
+  {
+    path: '/',
+    component: DashboardLayout,
+    redirect: '/admin/overview'
+  },
+  {
+    path: '/admin',
+    component: DashboardLayout,
+    redirect: '/admin/stats',
+    children: [
+      {
+        path: 'overview',
+        name: 'overview',
+        component: Overview
+      }
+    ]
+  },
+  { path: '*', component: NotFound }
+]
+
+export default routes
